112d0806d6f559d30edaed02b4d1b2b51ba1ec41
wudayu <869622470@qq.com>
2018-02-25 23:28:12 +0800

    first commit

---
+++ 1.py
@@ -0,0 +1,27 @@
+#coding=utf8
+import requests
+import itchat
+
+KEY = 'fafeee1e01344795a46fdae1a736258d'
+
+def get_response(msg):
+    apiUrl = 'http://www.tuling123.com/openapi/api'
+    data = {
+        'key'    : KEY,
+        'info'   : msg,
+        'userid' : 'wechat-robot',
+    }
+    try:
+        r = requests.post(apiUrl, data=data).json()
+        return r.get('text')
+    except:
+        return
+
+@itchat.msg_register(itchat.content.TEXT)
+def tuling_reply(msg):
+    defaultReply = 'I received: ' + msg['Text']
+    reply = get_response(msg['Text'])
+    return reply or defaultReply
+
+itchat.auto_login(hotReload=True)
+itchat.run()
